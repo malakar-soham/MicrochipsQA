673cbdba01f30522fa05c7a0cce19fbb06d29440
Soham Malakar <malakar_soham@outlook.com>
2019-07-31 13:06:06 +0530

    Update plotDecisionBoundary.m

--- plotDecisionBoundary.m
+++ plotDecisionBoundary.m
@@ -8,21 +8,20 @@ function plotDecisionBoundary(theta, X, y)
 %      intercept.
 %   2) MxN, N>3 matrix, where the first column is all-ones
 
-% Plot Data
+% Plotting Data
 plotData(X(:,2:3), y);
 hold on
 
 if size(X, 2) <= 3
-    % Only need 2 points to define a line, so choose two endpoints
+    % Only need 2 points to define a line, so choosing two endpoints
     plot_x = [min(X(:,2))-2,  max(X(:,2))+2];
 
-    % Calculate the decision boundary line
+    % Calculating the decision boundary line
     plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1));
 
-    % Plot, and adjust axes for better viewing
+    % Plotting, and adjusting axes for better viewing
     plot(plot_x, plot_y)
-    
-    % Legend, specific for the exercise
+
     legend('Admitted', 'Not admitted', 'Decision Boundary')
     axis([30, 100, 30, 100])
 else
@@ -39,8 +38,7 @@ else
     end
     z = z'; % important to transpose z before calling contour
 
-    % Plot z = 0
-    % Notice you need to specify the range [0, 0]
+    % Plotting z = 0
     contour(u, v, z, [0,0], 'LineWidth', 2)
 end
 hold off
